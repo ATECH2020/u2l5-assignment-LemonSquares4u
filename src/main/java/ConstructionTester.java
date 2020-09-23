999ecbfa4f5a522e16e9a78e750bf47893f4842d
Miguel Rivera Villareal <miguel.263282@nv.ccsd.net>
2020-09-23 17:42:22 +0000

    Began making variables and constructors for Construction

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -5,5 +5,16 @@ public class ConstructionTester
     public static void main(String[] args)
     {
         // Start here!
+        Scanner userInput = new Scanner(System.in);
+       
+       System.out.print("Enter the sales tax rate: ");
+       int numWindows;
+       int numBoards;
+       double taxRate;
+
+
+        Construction myConstruction = new Construction();
+
+
     }
 }
