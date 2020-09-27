c2128bb1a828b800213691e2703f7ee670455925
Miguel Rivera Villareal <miguel.263282@nv.ccsd.net>
2020-09-27 21:59:48 +0000

    Possily finished up Tester

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,17 +4,27 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
-        Scanner userInput = new Scanner(System.in);
        
-       System.out.print("Enter the sales tax rate: ");
+       // Start here!
+        Scanner userInput = new Scanner(System.in);
+
        int numWindows;
        int numBoards;
        double taxRate;
+       double total;
 
+        System.out.println("Enter the sales tax rate: ");
+        taxRate = userInput.nextDouble();
+        System.out.println("How many boards do you need?");
+        numBoards = userInput.nextInt();
+        System.out.println("How many windows do you need?"); 
+        numWindows = userInput.nextInt();
 
-        Construction myConstruction = new Construction();
-
-
+       Construction myConstruction = new Construction(8.0, 11.0, taxRate);
+        //myConstruction.lumberCost(numBoards); 
+        //myConstruction.windowCost(numWindows);
+        total = myConstruction.lumberCost(numBoards) + myConstruction.windowCost(numWindows); 
+        myConstruction.grandTotal(total);
+    }
     }
 }
